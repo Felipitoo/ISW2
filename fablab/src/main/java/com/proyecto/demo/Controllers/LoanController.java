28e36cf8386c1e6dbd26c95176c6a671123e3685
Ignacio Cofré <ignacio.cofre.14@sansano.usm.cl>
2019-07-20 15:22:02 -0400

    modificaciones a la vista loan

--- fablab/src/main/java/com/proyecto/demo/Controllers/LoanController.java
+++ fablab/src/main/java/com/proyecto/demo/Controllers/LoanController.java
@@ -6,6 +6,8 @@ import com.proyecto.demo.Services.ToolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
@@ -29,5 +31,17 @@ public class LoanController {
         mav.addObject("tools", list_tools);
         return mav;
     }
+    @PostMapping("/enviar")
+    public String GuardarPrestamo(@ModelAttribute ("full_name") String full_name,@ModelAttribute ("rut") String rut,
+    @ModelAttribute ("date") String date,@ModelAttribute ("tool_id") String tool_id){
+        System.out.println(full_name);
+        System.out.println(rut);
+        System.out.println(date);
+        System.out.println(tool_id);
+        return "redirect:/formulario/loan";
+    }
+
+
+
 }
 
---
+++ fablab/src/main/java/com/proyecto/demo/Entity/External.java
@@ -0,0 +1,4 @@
+package com.proyecto.demo.Entity;
+
+public class External {
+}
